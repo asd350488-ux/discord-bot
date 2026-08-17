34add5660b00c721e8596b757125130c7b45daa2
asd350488-ux <asd350488@gmail.com>
2026-08-17 18:03:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1872,17 +1872,10 @@ class CloseTicketView(discord.ui.View):
 
 # 🚀 啟動
 
-
 @bot.event
 async def on_ready():
 
     print(f"已登入：{bot.user}")
-
-    try:
-        synced = await bot.tree.sync()
-        print(f"✅ 已同步 {len(synced)} 個 Slash Commands")
-    except Exception as e:
-        print(f"❌ 指令同步失敗：{e}")
 
     # -------------------------
     # 永久 View（Persistent View）
@@ -1890,9 +1883,15 @@ async def on_ready():
 
     bot.add_view(ReviewPanelView())
     bot.add_view(ReviewManageView())
-    
+
     # 🌙 七夕限定盲盒
     setup_limited_lottery(bot)
+
+    try:
+        synced = await bot.tree.sync()
+        print(f"✅ 已同步 {len(synced)} 個 Slash Commands")
+    except Exception as e:
+        print(f"❌ 指令同步失敗：{e}")
     
     # 🎂 生日系統
     if not birthday_check.is_running():
