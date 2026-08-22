f2814e59010c9973b4757f41f2b84af8caa76e8f
asd350488-ux <asd350488@gmail.com>
2026-08-22 19:09:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from character_birthday import setup_character_birthday
 from systems.limited_lottery import setup_limited_lottery
-from character_test import setup_character_test
+from systems.character_test import setup_character_test
 from systems.mommy_roles import setup_mommy_roles
 from systems.character_exam import setup_character_exam
 from config import EXCLUDED_USERS
